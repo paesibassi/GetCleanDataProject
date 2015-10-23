93f726b3ac7e9746c58bbe4f66e5db92d2bd72f5
paesibassi <fedecalo@gmail.com>
2015-10-24 00:45:50 +0200

    added CodeBook and README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GetCleanDataProject
 Course Project results for the Getting and Cleaning Data course on Coursera
+
+run_analysis.R:
+
+0. download file and unzip in local folder, then import the fixed width files (using readr package, MUCH faster)
+1. merge the datasets (using data.table package for faster performance), and extract and filter column names (features)
+2. select only mean() and std() columns into a new data.table
+3. add activity names to labels
+4. clean descriptive column names
+5. calculate the mean for each variable for each activity and each subject
+6. export resulting data.table to X_summary.txt
--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,11 @@
-# The purpose of this project is to demonstrate your ability to collect, work
-# with, and clean a data set. The goal is to prepare tidy data that can be used
-# for later analysis. You will be graded by your peers on a series of yes/no
-# questions related to the project. You will be required to submit: 1) a tidy
-# data set as described below, 2) a link to a Github repository with your script
-# for performing the analysis, and 3) a code book that describes the variables,
-# the data, and any transformations or work that you performed to clean up the
-# data called CodeBook.md. You should also include a README.md in the repo with
-# your scripts. This repo explains how all of the scripts work and how they are
-# connected.
-# 
 # One of the most exciting areas in all of data science right now is wearable
 # computing - see for example this article . Companies like Fitbit, Nike, and
 # Jawbone Up are racing to develop the most advanced algorithms to attract new
 # users. The data linked to from the course website represent data collected
 # from the accelerometers from the Samsung Galaxy S smartphone. A full
 # description is available at the site where the data was obtained:
-# 
 # http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
-# 
 # Here are the data for the project:
-# 
 # https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 # 
 # You should create one R script called run_analysis.R that does the following:
@@ -33,7 +19,7 @@ library(readr)
 library(data.table)
 library(dplyr)
 
-# download file and unzip in local folder
+# 0. download file and unzip in local folder
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
               destfile = "dataset.zip", method = "curl")
 unzip(zipfile = "dataset.zip")
@@ -52,22 +38,31 @@ X_train <- data.table(subject_train, y_train, X_train) # merge subject, label an
 # 1. merge the datasets (using data.table package for faster performance)
 X_merge <- rbindlist(list(X_test, X_train))
 
-# extract and apply column names (features)
-# 4. apply descriptive column names
+# extract and filter column names (features)
 features <- read_delim("./UCI HAR Dataset/features.txt", delim = " ", col_names = FALSE)
-columns <- c("subject", "label", features[, 2])
+columns <- as.character(unlist(c("subject", "label", features[, 2])))
 colnames(X_merge) <- columns
 
-# 2. extract mean() and std() columns into a new data.table
+# 2. select only mean() and std() columns into a new data.table
 X_merge_meanstd <- X_merge[, grep("subject|label|mean\\(\\)|std\\(\\)", columns),
-                           with = FALSE] # with = FALSE allows for logical vector selection
+                           with = FALSE] # option "with = FALSE" allows for logical vector selection
 
 # 3. add activity names to labels
 labels <- as.data.table(read_delim("./UCI HAR Dataset/activity_labels.txt", delim = " ", col_names = FALSE))
 colnames(labels) <- c("label", "activity")
 X_merge_meanstd <- merge(labels, X_merge_meanstd, by = "label", all.y = TRUE)
 
+# 4. clean descriptive column names
+colnames(X_merge_meanstd) <- gsub("\\.", "", # trim dots
+                                  make.names( # removes special characters from column names
+                                    colnames(X_merge_meanstd),
+                                    unique = TRUE
+                                    )
+                                  )
 
-# 5. average for each variable for each activity and each subject
-setkey(X_merge_meanstd, activity, subject) # setkey automatically sort the data.table
+# 5. calculate the mean for each variable for each activity and each subject
+setkey(X_merge_meanstd, activity, subject)
 X_summary <- X_merge_meanstd[, lapply(.SD, sum), by = list(activity, subject), .SDcols = 4:69]
+
+# 6. export resulting data.table to X_summary.txt
+write.table(X_summary, file = "X_summary.txt", row.name=FALSE)
